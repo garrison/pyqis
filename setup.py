08b492b5771aee5718352d119529d1c6edaee16c
Jim Garrison <jim@garrison.cc>
2013-10-19 17:33:17 -0700

    setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='pyqis',
+    version='0.1dev',
+    author='James R. Garrison',
+    packages=[
+        'pyqis',
+    ],
+    install_requires=['numpy', 'ipython[notebook]'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+)
