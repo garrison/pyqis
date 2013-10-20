d86400b82c9c642cae1e2dbaa78abfe1fa5e50d1
Jim Garrison <jim@garrison.cc>
2013-10-19 23:10:19 -0700

    gates now operate desctructively on the current state.  implemented CNOT, CCNOT, Rtheta

--- pyqis/__init__.py
+++ pyqis/__init__.py
@@ -7,11 +7,12 @@ class QuantumBitMachine(object):
     def __init__(self, nqubits):
         assert isinstance(nqubits, Integral)
         self.nqubits = nqubits
-        state = [1.] + [0.] * (2 ** nqubits - 1)
-        self.state = np.array(state, dtype=complex)
+        self.state = np.zeros([2 ** nqubits], dtype=complex)
+        self.state[0] = 1.
 
     @classmethod
     def from_state(cls, nqubits, state):
+        assert len(state) == 2 ** nqubits
         rv = cls(nqubits)
         rv.state = state
         return rv
@@ -26,25 +27,27 @@ class QuantumBitMachine(object):
         bit = 1 << register
         for i in range(len(state)):
             state[i] = self.state[i ^ bit]
-        return QuantumBitMachine.from_state(self.nqubits, state)
+        self.state = state
+        return self
+
+    def __phase(self, register, phase):
+        bit = 1 << register
+        for i in range(len(self.state)):
+            if i & bit:
+                self.state[i] *= phase
+        return self
 
     def Z(self, register):
         """apply the PHASE gate on a given register"""
-        state = np.copy(self.state)
-        bit = 1 << register
-        for i in range(len(state)):
-            if i & bit:
-                state[i] = -1 * state[i]
-        return QuantumBitMachine.from_state(self.nqubits, state)
+        return self.__phase(register, -1.)
 
     def T(self, register):
         """apply the pi/8 gate on a given register"""
-        state = np.copy(self.state)
-        bit = 1 << register
-        for i in range(len(state)):
-            if i & bit:
-                state[i] = (1. + 1j) / np.sqrt(2) * state[i]
-        return QuantumBitMachine.from_state(self.nqubits, state)
+        return self.__phase(register, (1. + 1j) / np.sqrt(2))
+
+    def Rtheta(self, register, theta):
+        """apply an arbitrary phase rotation on a given register"""
+        return self.__phase(register, np.exp(1j * theta))
 
     def H(self, register):
         """apply the Hadamard gate on a given register"""
@@ -56,7 +59,29 @@ class QuantumBitMachine(object):
             else:
                 state[i] += 1 / np.sqrt(2) * self.state[i]
             state[i ^ bit] += 1 / np.sqrt(2) * self.state[i]
-        return QuantumBitMachine.from_state(self.nqubits, state)
+        self.state = state
+        return self
+
+    def CNOT(self, control_register, target_register):
+        state = np.copy(self.state)
+        control_bit = 1 << control_register
+        target_bit = 1 << target_register
+        for i in range(len(state)):
+            if i & control_bit:
+                state[i] = self.state[i ^ target_bit]
+        self.state = state
+        return self
+
+    def CCNOT(self, control_register1, control_register2, target_register):
+        state = np.copy(self.state)
+        control_bit1 = 1 << control_register1
+        control_bit2 = 1 << control_register2
+        target_bit = 1 << target_register
+        for i in range(len(state)):
+            if i & control_bit1 and i & control_bit2:
+                state[i] = self.state[i ^ target_bit]
+        self.state = state
+        return self
 
     def observe(self):
         cumsum = np.cumsum(np.square(np.abs(self.state)))
